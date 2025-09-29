7f997fe12bc89d3f66db34079ea57230991c3770
kiplimokelvin <kiplimo.26636@students.kyu.ac.ke>
2025-09-29 10:30:13 +0300

    Add files via upload

---
+++  p-details.c
@@ -0,0 +1,39 @@
+/*
+   Name: KELVIN 
+   REG NO: CT101/G/26636/25
+   DESCRIPTION: A C PROGRAM SHOWING MY HEIGHT BANK BALANCE AND PHONE NUMBER AND AGE 
+*/
+
+#include <stdio.h>
+
+int main() {
+    //Declare variables
+    float height;     
+    int age;          
+    double bank_balance;        
+    long long phone_number;      
+    
+       // prompt user to input height 
+    printf("Enter your height (in feet): ");
+    scanf("%f", &height);
+    
+    //prompt user to input age
+    printf("Enter your age: ");
+    scanf("%d",&age);
+    
+       //prompt user to input bank balance 
+    printf("Enter your bank balance: ");
+    scanf("%lf", &bank_balance);
+    
+    //prompt user to input phone number 
+    printf("Enter your phone number: ");
+    scanf("%lld", &phone_number);
+
+    // Display the entered values
+    printf("\nHeight: %.2f feet\n", height);
+    printf ("age: %d  years\n",age);
+    printf("Bank Balance: Ksh. %.2lf\n", bank_balance);
+    printf("Phone Number: %lld\n", phone_number);
+    
+    return 0;
+    }
