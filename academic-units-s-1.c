a92c2c968df49219405905da982296cf5bf4ba97
Kelvin kiplimo <kiplimo.26636@students.kyu.ac.ke>
2025-10-10 00:02:30 +0300

    Add files via upload

---
+++ academic-units-s-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // List of academic units offered this semester
+    printf("=== Academic Units Offered This Semester ===\n");
+    printf("1. Computer Programming\n");
+    printf("2. Data Structures and Algorithms\n");
+    printf("3. Discrete Mathematics\n");
+    printf("4. Database Systems\n");
+    printf("5. Operating Systems\n");
+    printf("6. Computer Networks\n");
+    printf("7. Software Engineering\n");
+    printf("8. Web Development\n");
+
+    return 0;
+}
